87ce53761d8f683482b982f8565c880797fb3dff
brohal97 <behal3622@gmail.com>
2026-01-21 16:01:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,30 @@ import os
 from datetime import datetime
 import pytz
 
-# Maklumat API
-API_ID = 26569722
-API_HASH = "809be041fdf8d87452174360e2d3122c"
+# Ambil token terus dari Railway
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-bot = Client("atv_bot_2026", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# Guna konfigurasi default yang lebih stabil
+bot = Client(
+    "atvpantaitimor_bot",
+    api_id=2040, 
+    api_hash="b18441a4833da9d0bd3395b54b7a7525",
+    bot_token=BOT_TOKEN
+)
 
 @bot.on_message(filters.photo)
 async def handle_photo(client, message):
     try:
         # Set masa Malaysia
         kl_pytz = pytz.timezone('Asia/Kuala_Lumpur')
-        tarikh_sekarang = datetime.now(kl_pytz).strftime('%d/%m/%Y %H:%M')
-        
-        # Simpan kapsyen asal jika ada
-        caption_asal = message.caption if message.caption else ""
+        waktu = datetime.now(kl_pytz).strftime('%d/%m/%Y %H:%M')
         
         # Padam gambar asal
         await message.delete()
         
-        # Balas dengan mesej baru
-        teks_balasan = (
-            f"✅ **REKOD DITERIMA**\n\n"
-            f"**Tarikh sekian :** {tarikh_sekarang}\n"
-            f"**Nota :** {caption_asal}"
-        )
-        
-        await client.send_message(chat_id=message.chat.id, text=teks_balasan)
-        
+        # Balas mesej
+        ayat = f"✅ **REKOD DITERIMA**\n\n**Tarikh sekian :** {waktu}"
+        await client.send_message(message.chat.id, ayat)
     except Exception as e:
         print(f"Ralat: {e}")
 
